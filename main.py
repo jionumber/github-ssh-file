1aa6e046d1ee41024a444bd97a9801e413956dfd
jionumber <jionumber98778@gmail.com>
2020-11-20 00:05:44 +0530

    question 6 commited

--- main.py
+++ main.py
@@ -17,3 +17,16 @@ elif(x%2==0):
     print("it is an even number.....")
 else:
     print("it is an odd number....")
+
+
+
+
+# question 6
+x = str(input("enter any word :"))
+rvs = x[::-1]
+print(rvs)
+if rvs == x:
+    print("it is a palindrome number")
+else:
+    print("it is a non palindrome number")
+
