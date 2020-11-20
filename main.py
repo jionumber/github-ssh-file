9dd7fd3722374310dfa1b890096683e1b3b14ab9
jionumber <jionumber98778@gmail.com>
2020-11-20 12:00:02 +0530

    question 11 commited

--- main.py
+++ main.py
@@ -50,3 +50,12 @@ elif(a==x):
     print("you guessed right")
 else:
     print("your number is too small")
+    
+#question 11  
+x = int(input("enter any number :"))
+for i in range(2,x):
+    if(x%i==0):
+        print("it is not a prime number")
+        break
+else:
+    print("it is a prime number")
